68ecaa863be57032529edb9c1549866fde235b8a
JacekJochemczyk <j.jochemczyk@gmail.com>
2024-11-20 23:05:08 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace PoleProstokata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Prostokat prostokat = new Prostokat();
+
+                prostokat.ReadData();
+                prostokat.ProcessData();
+                prostokat.ShowResults();
+
+                Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Prostokat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoleProstokata
+{
+    public class Prostokat
+    {
+        private double a; 
+        private double b; 
+        private double surfaceArea;
+
+        public void ReadData()
+        {
+            Console.Write("Podaj długość boku a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Podaj długość boku b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void ProcessData()
+        {
+            surfaceArea = a * b;
+        }
+
+        public void ShowResults()
+        {
+            Console.WriteLine("Wyniki:");
+            Console.WriteLine($"Bok a: {a:F2}");
+            Console.WriteLine($"Bok b: {b:F2}");
+            Console.WriteLine($"Pole prostokąta: {surfaceArea:F2}");
+        }
+    }
+}
